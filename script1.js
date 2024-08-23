4785118c3324f8291eb1991f1ce57d9d0b5a1789
yadavally sree vidya <100022229+Vinodchoudhary6016@users.noreply.github.com>
2024-08-23 19:13:28 +0530

    new

---
+++ script1.js
@@ -0,0 +1,17 @@
+// let user = "Admin";
+// if (user == "Admin") {
+//     console.log("login as admin");
+// } else if (user == "Employee") {
+//     console.log("login as Employee");
+// } else if (user == "super admin") {
+//     console.log("login as super admin");
+// } else if (user == "customer") {
+//     console.log("login as customer");
+// } else {
+//     console.log("unautho")
+// }
+for (i = 40; i <= 50; i++) {
+    if ((i - 40) % 2 == 0) {
+        console.log(`40X${i-40}=${40 * }`)
+    }
+}
